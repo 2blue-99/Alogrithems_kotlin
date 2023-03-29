6a5382e531064af7766b4e803bf54dee4466930f
pureum <lpm0831@naver.com>
2023-03-29 14:50:37 +0900

    1699, 11718

---
+++ 11047.kt
@@ -0,0 +1,31 @@
+package com.example.alogrithems
+
+/**
+ * 2023-03-29
+ * pureum
+ */
+
+fun main(){
+    val (num, gap) = readln().split(" ").map { it.toInt() }
+    val myArray = arrayListOf<Int>()
+    var maxCount = 0
+    for(i in 1..num){
+        var myGap = readln().toInt()
+        if(myGap>gap) break
+        myArray.add(myGap)
+    }
+
+    for( k in 0 until myArray.size) {
+        var myHap = 0
+        var count = 0
+        for (i in myArray.reversed().slice(k until myArray.size)) {
+            while (myHap + i <= gap) {
+                myHap += i
+                count++
+            }
+        }
+        if(k == 0) maxCount = count
+        else if(maxCount > count) maxCount = count
+    }
+    println(maxCount)
+}
