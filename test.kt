1111f7f8e0c3b1ce306186e1ee89f06881182510
pureum <lpm0831@naver.com>
2023-04-13 10:02:51 +0900

    1946

--- test.kt
+++ test.kt
@@ -1,24 +1,16 @@
 package com.example.alogrithems
 
+import kotlinx.coroutines.*
 import kotlin.math.max
 
-fun main(){
-    val nums = readln().toInt()
-    val score = Array(nums+1){0}
-    val hap = Array(nums+1){0}
-    for(i in 1..nums){
-        score[i] = readln().toInt()
-    }
+suspend fun main() {
+    val supervisor = SupervisorJob()
 
-    for(k in 1..nums){
-        when (k) {
-            1 -> hap[k] = score[k]
-            2 -> hap[k] = score[k]+score[k-1]
-            else -> {
-                hap[k] = max(hap[k-2]+score[k], hap[k-3]+score[k-1]+score[k])
-            }
+    CoroutineScope(Dispatchers.IO).launch {
+        val one = launch(Dispatchers.IO + supervisor ) {
+
         }
-    }
-    println(hap[nums])
+
+    }.join()
 }
 
