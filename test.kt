c3d67db836815be11ef4d8e122409842b94be8e5
pureum <lpm0831@naver.com>
2023-04-25 22:39:08 +0900

    소카 코테 문제

--- test.kt
+++ test.kt
@@ -1,16 +1,18 @@
 package com.example.alogrithems
 
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlin.math.max
 
-suspend fun main() {
-    val supervisor = SupervisorJob()
-
-    CoroutineScope(Dispatchers.IO).launch {
-        val one = launch(Dispatchers.IO + supervisor ) {
-
-        }
-
-    }.join()
+fun main() {
+    var gap = mutableMapOf<Int,List<Int>>()
+    gap.put(1, listOf(1,2,3,4))
+    gap.put(10, listOf(1,2,3,4))
+    gap.put(3, listOf(10,20,30,40))
+    gap.put(7, listOf(1,2,3,4))
+    gap.put(5, listOf(100,200,300,400))
+    println(gap)
+    gap = gap.toList().sortedBy { it.second[0] }.toMap().toMutableMap()
+    println(gap)
 }
-
