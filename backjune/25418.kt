0c5981e757451c663473d79e00876fc5ea466cef
pureum <lpm0831@naver.com>
2023-11-18 21:36:36 +0900

    [BFS] 백준 25418 큐 사용

--- backjune/25418.kt
+++ backjune/25418.kt
@@ -1,5 +1,7 @@
 package com.example.alogrithems.backjune
 
+import android.media.session.MediaSession.QueueItem
+
 /**
  * 2023-11-18
  * pureum
@@ -7,17 +9,42 @@ package com.example.alogrithems.backjune
 
 fun main() {
     val (a, k) = readln().split(" ").map { it.toInt() }
-    var now = k
-    var count = 0
+    var queue = ArrayDeque<List<Int>>()
+    var visitList = Array(1_000_001){false}
+    queue.addLast(listOf(a,0))
     while (true) {
-        if(now == a) break
-        if(now % 2 == 0 && now / 2 >= a){
-            now /= 2
-        }else{
-            now -= 1
+        if(queue.isEmpty()) break
+        val (nowA,count) = queue.removeFirst() // pop
+        if(visitList[nowA]) continue
+        if(nowA == k) {
+            println(count)
+            break
         }
-        count++
-        println(now)
+        if(k >= nowA * 2){
+            visitList[nowA] = true
+            queue.addLast(listOf(nowA*2, count+1))
+        }
+        if(k >= nowA+1){
+            visitList[nowA] = true
+            queue.addLast(listOf(nowA+1, count+1))
+        }
     }
-    println(count)
 }
+
+
+//fun main() {
+//    val (a, k) = readln().split(" ").map { it.toInt() }
+//    var now = k
+//    var count = 0
+//    while (true) {
+//        if(now == a) break
+//        if(now % 2 == 0 && now / 2 >= a){
+//            now /= 2
+//        }else{
+//            now -= 1
+//        }
+//        count++
+//        println(now)
+//    }
+//    println(count)
+//}
