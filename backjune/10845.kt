ff5fa862cbe3fde6f1e209b915fc7f0c65ebf298
pureum <lpm0831@naver.com>
2023-12-13 14:15:12 +0900

    [Queue] 백준 10845

---
+++ backjune/10845.kt
@@ -0,0 +1,42 @@
+package com.example.alogrithems.backjune
+
+/**
+ * 2023-12-13
+ * pureum
+ */
+
+fun main(){
+    val test = Test()
+
+    repeat(readln().toInt()){
+        val input = readln().split(" ")
+        when(input[0]){
+            "push" -> test.push(input[1].toInt())
+            "pop" -> println(test.pop())
+            "size" -> println(test.size())
+            "empty" -> println(test.empty())
+            "front" -> println(test.front())
+            else -> println(test.back())
+        }
+    }
+}
+
+class Test {
+    val arr = IntArray(10_000){0}
+    var top = 0
+    var tail = 0
+    fun push(num: Int){
+        arr[top] = num
+        top++
+    }
+    fun pop(): Int =
+        if(empty()==0) {
+            tail++
+            arr[tail-1]
+        }
+        else -1
+    fun size() = top-tail
+    fun empty(): Int = if(top==tail) 1 else 0
+    fun front():Int = if(empty()==0) arr[tail] else -1
+    fun back() = if(empty()==0) arr[top-1] else -1
+}
