44db526e99abaf94aca20ac8644a240bc6b6ce23
pureum <lpm0831@naver.com>
2023-12-14 21:57:41 +0900

    [BFS] 백준 1926

---
+++ backjune/1926.kt
@@ -0,0 +1,46 @@
+package com.example.alogrithems.backjune
+
+/**
+ * 2023-12-14
+ * pureum
+ */
+
+
+
+fun main(){
+    val (maxh, maxw) = readln().split(" ").map { it.toInt() }
+    var arr = Array(maxh){ IntArray(maxw) }
+    arr.forEachIndexed { index, _ -> arr[index] = readln().split(" ").map { list -> list.toInt() }.toIntArray() }
+    var vArr = Array(maxh){ BooleanArray(maxw){false} }
+    var max = 0
+    var num = 0
+    val queue = ArrayDeque<List<Int>>()
+
+    for(h in 0 until maxh){
+        for(w in 0 until maxw){
+            if(!vArr[h][w] && arr[h][w]==1) {
+                var count = 0
+                queue.addLast(listOf(h, w, 0))
+
+                while (queue.isNotEmpty()) {
+                    var (nh, nw) = queue.removeFirst()
+                    if (nh < 0 || nw < 0 || nh >= maxh || nw >= maxw) continue
+                    if (vArr[nh][nw]) continue
+                    if (arr[nh][nw] == 0) continue
+                    vArr[nh][nw] = true
+                    count++
+
+                    queue.addLast(listOf(nh + 1, nw))
+                    queue.addLast(listOf(nh - 1, nw))
+                    queue.addLast(listOf(nh, nw + 1))
+                    queue.addLast(listOf(nh, nw - 1))
+                }
+                if(max<count) max = count
+                num ++
+            }
+        }
+    }
+    println(num)
+    println(max)
+}
+
---
+++ backjune/4949.kt
@@ -0,0 +1,39 @@
+package com.example.alogrithems.backjune
+
+import java.util.Stack
+
+/**
+ * 2023-12-13
+ * pureum
+ */
+
+fun main(){
+    loop@ while(true){
+        val stack = Stack<Char>()
+        val input = readln()
+        if(input==".") break
+
+        for(it in input) {
+            when(it){
+                '(' -> { stack.push(')') }
+                '[' -> { stack.push(']') }
+                ']' -> {
+                    if(stack.isEmpty() || stack.pop() != ']') {
+                        println("no")
+                        continue@loop
+                    }
+                }
+                ')' ->  {
+                    if(stack.isEmpty() || stack.pop() != ')') {
+                        println("no")
+                        continue@loop
+                    }
+                }
+                '.' -> break
+                else -> {}
+            }
+        }
+        if(stack.isEmpty()) println("yes")
+        else println("no")
+    }
+}
