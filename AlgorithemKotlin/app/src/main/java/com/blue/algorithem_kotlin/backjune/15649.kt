84bbc329c61e11dcb6ec8586d3d9d95ca638f75d
pureum <lpm0831@naver.com>
2024-01-17 22:44:58 +0900

    [back tracking] 백준 15649 - 백트래킹 적용

--- AlgorithemKotlin/app/src/main/java/com/blue/algorithem_kotlin/backjune/15649.kt
+++ AlgorithemKotlin/app/src/main/java/com/blue/algorithem_kotlin/backjune/15649.kt
@@ -1,22 +1,55 @@
 package com.blue.algorithem_kotlin.backjune
 
-fun main() {
-    val (n, m) = readln().split(" ").map { it.toInt() }
-    val resultList = mutableListOf<Int>()
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
 
-    fun dfs(num: Int, list: MutableList<Int>) {
-        if (list.size == m) {
-            list.forEach { print("$it ") }
-            println()
+fun main() {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val (n, m) = readln().split(" ").map { it.toInt() }
+    val arr = Array(10){0}
+    val vArr = Array(10){false}
+
+    fun dfs(k: Int) {
+        if (k == m) {
+            for(i in 0 until m){
+                bw.write("${arr[i]} ")
+            }
+            bw.newLine()
             return
         }
-        for (i in 1..num) {
-            if (list.contains(i)) continue
-            val newList = list.toMutableList()
-            newList.add(i)
-            dfs(num, newList)
+        for (i in 1..n ) {
+            if (!vArr[i]) {
+                arr[k] = i
+                vArr[i] = true
+                dfs(k + 1)
+                vArr[i] = false
+            }
         }
     }
-
-    dfs(n, resultList)
+    dfs(0)
+    bw.flush()
+    bw.close()
 }
+
+//fun main() {
+//    val bw = BufferedWriter(OutputStreamWriter(System.out))
+//    val (n, m) = readln().split(" ").map { it.toInt() }
+//    val resultList = mutableListOf<Int>()
+//
+//    fun dfs(num: Int, list: MutableList<Int>) {
+//        if (list.size == m) {
+//            list.forEach { bw.write("$it ") }
+//            bw.newLine()
+//            return
+//        }
+//        for (i in 1..num) {
+//            if (list.contains(i)) continue
+//            val newList = list.toMutableList()
+//            newList.add(i)
+//            dfs(num, newList)
+//        }
+//    }
+//    dfs(n, resultList)
+//    bw.flush()
+//    bw.close()
+//}
