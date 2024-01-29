3ffe7d793aa7f9812cab1f18df6dea4121a23f54
pureum <lpm0831@naver.com>
2024-01-29 11:31:27 +0900

    [simulation] 백준 18808 성공

--- AlgorithemKotlin/app/src/main/java/com/blue/algorithem_kotlin/backjune/18808.kt
+++ AlgorithemKotlin/app/src/main/java/com/blue/algorithem_kotlin/backjune/18808.kt
@@ -3,44 +3,30 @@ package com.blue.algorithem_kotlin.backjune
 lateinit var noteBook: Array<IntArray>
 fun main() {
     val (n, m, k) = readln().split(" ").map { it.toInt() }
+    var result = 0
     noteBook = Array(n) { IntArray(m) }
-    var h = 0
-    var w = 0
+
     repeat(k) {
         val (row, col) = readln().split(" ").map { it.toInt() }
         var sticker = Array(row) { IntArray(col) }
-        var tempSticker = Array(row) { IntArray(col) }
 
         repeat(row) {
             val gap = readln().split(" ").map { it.toInt() }.toIntArray()
             sticker[it] = gap
-            tempSticker[it] = gap
         }
-        println("이번에 들어온 스티커")
-        sticker.forEach { println(it.contentToString()) }
-
-        loop@ while (h<n) {
-            while (w < m) {
-                if(noteBook[h][w] == 0)
-                    for (i in 0..3) {
-                        if (checkAttach(sticker, h, w)) {
-                            w += attach(sticker, h, w)
-                            break@loop
-                        } else {
-                            if (i == 3) break
-//                        if(!checkSticker(sticker,h,w)) continue
-                            sticker = rotation(sticker)
-                        }
+        loop@for(rotation in 0..3) {
+             for (h in 0 until n) {
+                for (w in 0 until m) {
+                    if (checkAttach(sticker, h, w)) {
+                        result += attach(sticker, h, w)
+                        break@loop
                     }
-                w += 1
+                }
             }
-            w = 0
-            h += 1
+            sticker = rotation(sticker)
         }
-        tempSticker = sticker
-        noteBook.forEach { println(it.contentToString()) }
-        println()
     }
+    println(result)
 }
 
 fun checkAttach(sticker: Array<IntArray>, sh: Int, sw: Int): Boolean {
@@ -57,29 +43,17 @@ fun checkAttach(sticker: Array<IntArray>, sh: Int, sw: Int): Boolean {
     return true
 }
 
-//fun checkSticker(sticker: Array<IntArray>, sh: Int, sw: Int): Boolean {
-//    return if(noteBook[0].size-sh < sticker.size || noteBook.size - sw < sticker[0].size )
-//        false
-//    else
-//        true
-//    if (noteBook[0].size - sh < sticker.size || noteBook.size - sw < sticker[0].size) return false
-//
-//    for (h in sh until sticker.size) {
-//        for (w in sw until sticker[0].size) {
-//            if (noteBook[h][w] == 1) return false
-//        }
-//    }
-//    return true
-//}
-
 fun attach(sticker: Array<IntArray>, sh: Int, sw: Int): Int {
+    var cnt = 0
     for (h in 0 until sticker.size) {
         for (w in 0 until sticker[0].size) {
-            if (sticker[h][w] == 1)
-                noteBook[h+sh][w+sw] = 1
+            if (sticker[h][w] == 1) {
+                noteBook[h + sh][w + sw] = 1
+                cnt++
+            }
         }
     }
-    return noteBook[sh].reversed().indexOfFirst { it==1 }
+    return cnt
 }
 
 fun rotation(sticker: Array<IntArray>): Array<IntArray> {
@@ -94,6 +68,5 @@ fun rotation(sticker: Array<IntArray>): Array<IntArray> {
             rotatedSticker[w][col - 1 - h] = sticker[h][w]
         }
     }
-    rotatedSticker.forEach { println(it.contentToString()) }
     return rotatedSticker
 }
