bf79c71cb9f7f9ea6da961fda246cfaa908efeff
pureum <lpm0831@naver.com>
2024-04-05 00:01:15 +0900

    [Sort] 백준 10814 수정

--- backjune/10814.kt
+++ backjune/10814.kt
@@ -1,14 +1,14 @@
 package com.blue.algorithem_kotlin.backjune
 
 fun main() {
-    val map = mutableMapOf<Pair<Int, String>, Int>()
+    val list = mutableListOf<Pair<Pair<Int, String>, Int>>()
     val n = readln().toInt()
     for (i in 0 until n) {
         val input = readln().split(" ")
-        val pair = Pair(input.first().toInt(), input.last())
-        map[pair] = map.getOrDefault(pair, i)
+        val pair = Pair(Pair(input.first().toInt(), input.last()), i)
+        list.add(pair)
     }
-    map.toList().sortedWith(compareBy({ it.first.first }, { it.second })).forEach {
+    list.sortedWith(compareBy({ it.first.first }, { it.second })).forEach {
         println(it.first.toList().joinToString(" "))
     }
 }
