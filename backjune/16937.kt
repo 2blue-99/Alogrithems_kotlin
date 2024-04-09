d425e04e55da58ff9fe7094c4db102fabfcd94d0
pureum <lpm0831@naver.com>
2024-04-10 00:16:32 +0900

    [Simulation] 백준 16937

---
+++ backjune/16937.kt
@@ -0,0 +1,47 @@
+package com.blue.algorithem_kotlin.backjune
+
+import java.util.LinkedList
+import java.util.Queue
+
+fun main() {
+    val (h, w) = readln().split(" ").map { it.toInt() }
+    val n = readln().toInt()
+
+    val list = mutableListOf<List<Int>>()
+    val result = mutableListOf<Int>()
+
+    repeat(n) { list.add(readln().split(" ").map { it.toInt() }) }
+
+    fun check(a: List<Int>, b: List<Int>): Boolean {
+        if (a.first() + b.first() <= w && kotlin.math.max(a.last(), b.last()) <= h) return true
+        if (kotlin.math.max(a.first(), b.first()) <= w && a.last() + b.last() <= h) return true
+        return false
+    }
+
+    for (i in list.indices) {
+        for (j in i + 1 until list.size) {
+            var a = list[i] //2 1
+            var b = list[j] //3 4
+            var sum = a.first() * a.last() + b.first() * b.last()
+
+            if (check(a, b)) {
+                result.add(sum)
+                continue
+            }
+            if (check(a.reversed(), b)) {
+                result.add(sum)
+                continue
+            }
+            if (check(a, b.reversed())) {
+                result.add(sum)
+                continue
+            }
+            if (check(a.reversed(), b.reversed())) {
+                result.add(sum)
+                continue
+            }
+        }
+    }
+    if (result.size == 0) println(0)
+    else println(result.max())
+}
