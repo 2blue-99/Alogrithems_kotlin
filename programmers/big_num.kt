1857a5323c88ce2321f501f921e18ae72c00275a
pureum <lpm0831@naver.com>
2024-04-11 10:56:23 +0900

    [Sort] 프로그래머스 가장 큰 수 - 재풀이

--- programmers/big_num.kt
+++ programmers/big_num.kt
@@ -1,15 +1,18 @@
 package com.blue.algorithem_kotlin.programmers
 
-fun main() {
-    val temp = intArrayOf(4,41,355)
-    val list = mutableListOf<String>()
-
-    temp.forEach { list.add(it.toString()) }
-    list.sortedWith{ a, b ->
-        when{
-            a.length==b.length -> b.compareTo(a)
-            else -> (b+a).compareTo(a+b)
-        }
+class BigNum {
+    fun solution(numbers: IntArray): String {
+        // 두 숫자 길이가 같은 경우엔 그냥 비교하고
+        // 두 숫자 길이가 다른 경우엔 ab ba를 비교해서 더 큰걸로
+        val list = numbers.map{it.toString()}
+            .sortedWith(Comparator{a,b ->
+                when{
+                    a.length == b.length -> b.compareTo(a)
+                    else -> (b+a).compareTo(a+b)
+                }
+            })
+            .joinToString("")
+        return if(list.first()=='0') "0"
+        else list
     }
-    println(list)
 }
