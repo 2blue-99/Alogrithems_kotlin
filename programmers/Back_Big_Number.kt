dda9192c15957bb0298848f41ef9c06fab578c5c
pureum <lpm0831@naver.com>
2024-07-05 01:06:22 +0900

    [Stack] 프로그래머스 - 뒤에 있는 큰 수 찾기

--- programmers/Back_Big_Number.kt
+++ programmers/Back_Big_Number.kt
@@ -1,5 +1,7 @@
 package com.blue.algorithem_kotlin.programmers
 
+import java.util.*
+
 fun main(){
     Back_Big_Number().solution(intArrayOf())
 }
@@ -7,15 +9,21 @@ fun main(){
 class Back_Big_Number {
     fun solution(numbers: IntArray): IntArray {
         var answer: IntArray = IntArray(numbers.size){-1}
-        for(i in numbers.indices){
-            if(i < numbers.size-1){
-                for(j in i+1 until numbers.size){
-                    if(numbers[i] < numbers[j]) {
-                        answer[i] = numbers[j]
+        val stack: Stack<Int> = Stack()
+        for(i in (numbers.size-1) downTo 0){
+            if(stack.isEmpty()){
+                answer[i] = -1
+            }else{
+                while(stack.isNotEmpty()){
+                    if(stack.peek() > numbers[i]){
+                        answer[i] = stack.peek()
                         break
+                    }else{
+                        stack.pop()
                     }
                 }
             }
+            stack.push(numbers[i])
         }
         return answer
     }
