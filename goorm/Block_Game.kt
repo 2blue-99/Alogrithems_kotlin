a3cd779af2b58c1abec774f5cff89616746beaac
pureum <lpm0831@naver.com>
2024-07-14 23:57:09 +0900

    [Stack] 구름 - 블록 게임2

--- goorm/Block_Game.kt
+++ goorm/Block_Game.kt
@@ -47,57 +47,57 @@ fun main(args: Array<String>) {
     println(score)
 }
 
-//fun main(args: Array<String>) {
-//
-//    val input = readLine()!!.toInt()
-//    val directionList = readLine()!!.toList()
-//    val rankList = readLine()!!.split(" ").map{it.toInt()}
-//    val arr = Array(input*2+1){IntArray(input*2+1)}
-//    var rank = 1
-//    val stack: Stack<Triple<Int, Int, Int>> = Stack()
-//    var currentIndex = 1
-//
-//    // 시작 위치
-//    var (a,b) = listOf(input+1,input+1)
-//    arr[a][b] = 1
-//    stack.push(Triple(a,b,1))
-//
-//    fun back(cnt: Int){
-//        repeat(cnt){
-//            val (ca,cb,cRank) = stack.pop()
-//            arr[ca][cb] = 0
-//            rank-=cRank
-//            currentIndex--
-//        }
-//    }
-//
-//    for(i in directionList.indices){
-//        if(directionList[i] == 'L'){
-//
-//            if(arr[a][b-1] != 0) back(currentIndex - arr[a][b-1] + 1)
-//            b-=1
-//
-//        }else if(directionList[i] == 'R'){
-//
-//            if(arr[a][b+1] != 0) back(currentIndex - arr[a][b+1] + 1)
-//            b+=1
-//
-//        }else if(directionList[i] == 'U'){
-//
-//            if(arr[a-1][b] != 0) back(currentIndex - arr[a-1][b] + 1)
-//            a-=1
-//
-//        }else if(directionList[i] == 'D'){
-//            if(arr[a+1][b] != 0) back(currentIndex-arr[a+1][b] + 1)
-//            a+=1
-//        }
-//
-//        currentIndex++
-//        arr[a][b] = currentIndex
-//        rank+=rankList[i]
-//        stack.add(Triple(a,b,rankList[i]))
-//
-//    }
-//
-//    print(rank)
-//}
+fun main(args: Array<String>) {
+
+    val input = readLine()!!.toInt()
+    val directionList = readLine()!!.toList()
+    val rankList = readLine()!!.split(" ").map{it.toInt()}
+    val arr = Array(input*2+1){IntArray(input*2+1)}
+    var rank = 1
+    val stack: Stack<Triple<Int, Int, Int>> = Stack()
+    var currentIndex = 1
+
+    // 시작 위치
+    var (a,b) = listOf(input+1,input+1)
+    arr[a][b] = 1
+    stack.push(Triple(a,b,1))
+
+    fun back(cnt: Int){
+        repeat(cnt){
+            val (ca,cb,cRank) = stack.pop()
+            arr[ca][cb] = 0
+            rank-=cRank
+            currentIndex--
+        }
+    }
+
+    for(i in directionList.indices){
+        if(directionList[i] == 'L'){
+
+            if(arr[a][b-1] != 0) back(currentIndex - arr[a][b-1] + 1)
+            b-=1
+
+        }else if(directionList[i] == 'R'){
+
+            if(arr[a][b+1] != 0) back(currentIndex - arr[a][b+1] + 1)
+            b+=1
+
+        }else if(directionList[i] == 'U'){
+
+            if(arr[a-1][b] != 0) back(currentIndex - arr[a-1][b] + 1)
+            a-=1
+
+        }else if(directionList[i] == 'D'){
+            if(arr[a+1][b] != 0) back(currentIndex-arr[a+1][b] + 1)
+            a+=1
+        }
+
+        currentIndex++
+        arr[a][b] = currentIndex
+        rank+=rankList[i]
+        stack.add(Triple(a,b,rankList[i]))
+
+    }
+
+    print(rank)
+}
